5631e8aee0e6c4f8d7af7516ad64311d496f6367
jhulter <79738742+jhulter@users.noreply.github.com>
2024-06-17 18:44:49 -0700

    Update question_3.py

--- question_3.py
+++ question_3.py
@@ -6,3 +6,6 @@ variable_c = 3.14
 variable_d = True
 
 print("variable_a is type", type(variable_a))
+print("variable_b is type", type(variable_b))
+print("variable_c is type", type(variable_c))
+print("variable_d is type", type(variable_d))
